2c15ec19729333789689657f4f1949cd9d2bd6a2
braian <brasanluc123@gmail.com>
2024-04-16 14:26:09 -0300

    clas 17; Create an interface where you have one type of log and the other type of logout as reducers.

--- 01-react-native-fundamentos/src/store/auth.store.ts
+++ 01-react-native-fundamentos/src/store/auth.store.ts
@@ -1,16 +1,37 @@
 import { create } from "zustand";
 
 interface AuthState {
-  status: "authenticated " | "unauthenticated" | "cheking";
+  status: "authenticated " | "unauthenticated" | "checking";
   token?: string;
   user?: {
     name: string;
     email: string;
   };
+
+  login: (email: string, password: string) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
-  status: "cheking",
+  status: "checking",
   token: undefined,
   user: undefined,
+  login: (email: string, password: string) => {
+    set({
+      status: "authenticated ",
+      token: "ABC123",
+      user: {
+        name: "Braian Lucero ",
+        email: email,
+      },
+    });
+  },
+
+  logout: () => {
+    set({
+      status: "unauthenticated",
+      token: undefined,
+      user: undefined,
+    });
+  },
 }));
