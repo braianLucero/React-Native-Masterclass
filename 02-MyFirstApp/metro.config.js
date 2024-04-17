81663249a27a4fbeb4ec2de849babd673f88af82
braian <brasanluc123@gmail.com>
2024-04-17 15:05:26 -0300

    clas 45 ; studying the files and directories of our app

--- 02-MyFirstApp/metro.config.js
+++ 02-MyFirstApp/metro.config.js
@@ -9,3 +9,5 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+// para gestionar y transformar los archivos JavaScript de tu aplicación. Metro se encarga de compilar tu código, manejar las dependencias y preparar todo para que pueda ser ejecutado en un dispositivo móvil o en un emulador.
