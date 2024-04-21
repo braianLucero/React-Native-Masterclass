23ec941244834c5cdcb1efc5ffcef03c90a7f351
braian <brasanluc123@gmail.com>
2024-04-21 18:57:22 -0300

    clas 100 ;  creating project files and directories

---
+++ 04-NavigationApp/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+};
