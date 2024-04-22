00c08501eb837443d23fa423adb07ce32ab38ca6
braian <brasanluc123@gmail.com>
2024-04-21 21:57:07 -0300

    clas 103;navigating between screens

--- 04-NavigationApp/src/presentation/screen/home/HomeScreen.tsx
+++ 04-NavigationApp/src/presentation/screen/home/HomeScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {globalStyle} from '../../theme/theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {useNavigation} from '@react-navigation/native';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalStyle.container}>
+      <PrimaryButton
+        onPress={() => navigation.navigate('Products' as never)}
+        label="Products"
+      />
+
+      <PrimaryButton
+        onPress={() => navigation.navigate('Settings' as never)}
+        label="Settings"
+      />
     </View>
   );
 };
+//
