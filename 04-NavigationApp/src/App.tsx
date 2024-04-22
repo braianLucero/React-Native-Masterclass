8adf0dc4b94005ff2e2e503e84f0545a8637dfa5
braian <brasanluc123@gmail.com>
2024-04-22 15:10:08 -0300

    clas 109 ; configured Navigate drawer

--- 04-NavigationApp/src/App.tsx
+++ 04-NavigationApp/src/App.tsx
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {StackNavigator} from './presentation/routes/StackNavigator';
+import {DrawerNavigator} from './presentation/routes/DrawerNavigator';
+// import {StackNavigator} from './presentation/routes/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };
