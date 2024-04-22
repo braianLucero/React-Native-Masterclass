9c612fa61e2dde70d577c2a4da26aaaccdc7223d
braian <brasanluc123@gmail.com>
2024-04-21 22:52:35 -0300

    clas 105 ; seeing how flatList works with product screens

--- 04-NavigationApp/src/presentation/routes/StackNavigator.tsx
+++ 04-NavigationApp/src/presentation/routes/StackNavigator.tsx
@@ -2,6 +2,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screen/home/HomeScreen';
 import {ProductsScreen} from '../screen/products/ProductsScreen';
 import {SettingsScreen} from '../screen/settings/SettingsScreen';
+import {ProductScreen} from '../screen/products/ProductScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +16,7 @@ export const StackNavigator = () => {
         },
       }}>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
