276d19940eb80acb3f17818ac174961d157d28a4
braian <brasanluc123@gmail.com>
2024-04-22 22:22:52 -0300

    clas 121 ; applying hamburger menu

--- 04-NavigationApp/src/presentation/screen/tabs/Tab2Screen.tsx
+++ 04-NavigationApp/src/presentation/screen/tabs/Tab2Screen.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {HamburguerMenu} from '../../components/shared/HamburguerMenu';
 
 export const Tab2Screen = () => {
   return (
     <View>
+      <HamburguerMenu />
+
       <Text>Tab 2 Screen </Text>
     </View>
   );
