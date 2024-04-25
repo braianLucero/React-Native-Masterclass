9e6a12e5bd809d3b4ac07be7d51e26508b77d929
braian <brasanluc123@gmail.com>
2024-04-24 22:46:50 -0300

    clas 158 ;finishing the carousel and navigation

--- src/presentation/components/MoviesPoster.tsx
+++ src/presentation/components/MoviesPoster.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, Pressable, StyleSheet, View} from 'react-native';
 import {Movie} from '../../core/entities/movie.entity';
+import {
+  NavigationContainerProps,
+  useNavigation,
+} from '@react-navigation/native';
+import {RootStackParams, Navigation} from '../navigations/Navigation';
 interface Props {
-  movies: Movie;
+  movie: Movie;
+  height?: number;
+  width?: number;
 }
-export const MoviesPoster = ({movies}: Props) => {
+export const MoviesPoster = ({movie, height = 420, width = 300}: Props) => {
+  const navigation = useNavigation<NavigationContainerProps<RootStackParams>>();
   return (
-    <View>
-      <Image source={{uri: movie.poster}} />
-    </View>
+    <Pressable
+      onPress={() => navigation.navigate('Detail', {movieId: movie.id})}>
+      <View>
+        <Image source={{uri: movie.poster}} />
+      </View>
+    </Pressable>
   );
 };
 
